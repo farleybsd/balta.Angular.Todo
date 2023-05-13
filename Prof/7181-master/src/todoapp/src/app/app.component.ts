90d4d9b8ebef736e861af22fe7c186dab135de74
farleybsd <farley.t.i@hotmail.com>
2023-05-13 10:34:22 -0300

    Finish

--- Prof/7181-master/src/todoapp/src/app/app.component.ts
+++ Prof/7181-master/src/todoapp/src/app/app.component.ts
@@ -1,46 +1,47 @@
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Todo } from './../models/todo.model';
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // <app-root>
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public mode: String = 'list';
   public todos: Todo[] = [];
-  public title: string = 'Minhas Tarefas';
-  public form =  new FormGroup({
-    
-  });
+  public form: FormGroup;
+  public mode: String = 'list';
 
-  constructor(private fb : FormBuilder) {
-
+  constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
-      title:['',Validators.compose([
+      title: ['', Validators.compose([
         Validators.minLength(3),
         Validators.maxLength(60),
-        Validators.required
+        Validators.required,
       ])]
     });
+
     this.load();
   }
 
-  add(){
-    const title = this.form.controls['title'].value;
-    const id = this.todos.length + 1;
-    this.todos.push(new Todo(id,title, false));
-    this.save()
-    this.clear();
+  changeMode(mode: String) {
+    this.mode = mode;
   }
 
-  clear(){
+  add() {
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
+    this.save();
+    this.clear();
+    this.changeMode('list');
+  }
+
+  clear() {
     this.form.reset();
   }
 
   remove(todo: Todo) {
-
     const index = this.todos.indexOf(todo);
     if (index !== -1) {
       this.todos.splice(index, 1);
@@ -58,18 +59,17 @@ export class AppComponent {
     this.save();
   }
 
-  save(){
+  save() {
     const data = JSON.stringify(this.todos);
-    localStorage.setItem('todos',data);
-    this.mode = 'list';
+    localStorage.setItem('todos', data);
   }
 
-  load(){
+  load() {
     const data = localStorage.getItem('todos');
-    this.todos = JSON.parse(data!);
+    if (data) {
+      this.todos = JSON.parse(data);
+    } else {
+      this.todos = [];
+    }
   }
-  changeMode(mode: String) {
-    this.mode = mode;
-  }
-
 }
