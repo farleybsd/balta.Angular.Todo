d913e6aa20dd4352b47bb1dcc49ba63b97adda75
farleybsd <farley.t.i@hotmail.com>
2023-05-12 23:38:58 -0300

    Validacoes

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Todo } from './../models/todo.model';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +8,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+
+  public todos: Todo[] = [];
+  public title: string = 'Minas Tarefas';
+  public form =  new FormGroup({
+    
+  });
+
+  constructor(private fb : FormBuilder) {
+
+    this.form = this.fb.group({
+      title:['',Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    });
+
+    this.todos.push(new Todo(1, 'Passear Com o Doguinho <3', false));
+    this.todos.push(new Todo(2, 'Ir Ao Supermecado <3', false));
+    this.todos.push(new Todo(3, 'Cortar O Cabelo <3', true));
+  }
+
+  remove(todo: Todo) {
+
+    const index = this.todos.indexOf(todo);
+    if (index !== -1) {
+      this.todos.splice(index, 1)
+    }
+  }
+
+  markAsDone(todo: Todo) {
+    todo.done = true
+  }
+
+  markAsUndone(todo: Todo) {
+    todo.done = false
+  }
 }
